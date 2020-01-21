b263a9cd2a0ae5c273d305df27322165922f0809
y0d3n <bjay1290115@gn.iwasaki.ac.jp>
2020-01-21 23:35:21 +0900

    Rework

--- main.go
+++ main.go
@@ -16,6 +16,6 @@ func main() {
 func solve(buf []string) {
 	a, _ := strconv.Atoi(buf[0])
 	b, _ := strconv.Atoi(buf[1])
-	reply = strconv.Itoa(a ^ b)
+	reply = strconv.Itoa(6 - a - b)
 	fmt.Println(reply)
 }
